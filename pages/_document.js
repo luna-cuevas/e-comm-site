e9e87cf8d2e8a1f289a5343ebae3c57bb7c23e21
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2022-05-10 19:23:46 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,9 @@ export default function Document() {
       <Head>
         <link rel="shortcut icon" href="/cauldron.svg" />
         <meta name="description" content="Your one-stop shop for glass art, macrames, and hand-crafted arts." />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin  />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
         <Main />
