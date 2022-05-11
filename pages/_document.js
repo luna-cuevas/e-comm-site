9eec59db366c69b983520deb0ca7a01badebd4c8
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2022-05-11 00:59:15 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-B2SHBWKE5F"></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-B2SHBWKE5F');`}
+        </script>
         <link rel="shortcut icon" href="/cauldron.svg" />
         <meta name="description" content="Your one-stop shop for glass art, macrames, and hand-crafted arts." />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
